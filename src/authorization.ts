b4a38dccab14d120052d698ca8d4e28751faaeb1
Adam Bukowski <bukowski.adam@gmail.com>
2022-10-09 16:07:11 +0200

    feat: use time safe compare in auth

--- src/authorization.ts
+++ src/authorization.ts
@@ -1,8 +1,13 @@
 import type { VercelRequest } from "@vercel/node";
+import scmp from "scmp";
+
 import { AUTH_TOKEN, IS_DEVELOPMENT } from "./config";
 
 export function isAuthorized(req: VercelRequest): boolean {
   const { auth = "" } = req.query || {};
 
-  return auth === AUTH_TOKEN || IS_DEVELOPMENT;
+  const expected = Buffer.from(AUTH_TOKEN);
+  const provided = Buffer.from(auth.toString());
+
+  return scmp(expected, provided) || IS_DEVELOPMENT;
 }
