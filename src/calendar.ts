143f24fd03ae1c912d07b83802e442027b4eab02
Adam Bukowski <bukowski.adam@gmail.com>
2022-10-21 12:16:25 +0200

    feat: add types to calendar

--- src/calendar.ts
+++ src/calendar.ts
@@ -3,6 +3,13 @@ import axios from "axios";
 import { CALENDAR_EVENTS_URL, IS_DEVELOPMENT, TIME_ZONE } from "../src/config";
 import { calendarMockData } from "./mocks/calendar";
 
+type CalendarApiDayData = {
+  day: Date;
+  allDay: Array<{ title: string; calendar: string }>;
+  time: Array<{ title: string; calendar: string; start: Date; end: Date }>;
+};
+type CalendarApiData = Array<CalendarApiDayData>;
+
 export async function getCalendarData() {
   if (IS_DEVELOPMENT) {
     return calendarMockData.short;
@@ -12,29 +19,32 @@ export async function getCalendarData() {
     return [];
   }
 
-  return (await axios.get(CALENDAR_EVENTS_URL)).data.map((row) => {
-    row.day = new Date(row.day).toLocaleString("pl-PL", {
-      timeZone: TIME_ZONE,
-      weekday: "long",
-      month: "numeric",
-      day: "numeric",
-    });
+  return (await axios.get<CalendarApiData>(CALENDAR_EVENTS_URL)).data.map(
+    (row) => ({
+      ...row,
 
-    row.time.forEach((ev) => {
-      ev.start = new Date(ev.start).toLocaleString("pl-PL", {
+      day: new Date(row.day).toLocaleString("pl-PL", {
         timeZone: TIME_ZONE,
-        hour: "2-digit",
-        minute: "2-digit",
-      });
-      ev.end = new Date(ev.end).toLocaleString("pl-PL", {
-        timeZone: TIME_ZONE,
-        hour: "2-digit",
-        minute: "2-digit",
-      });
-    });
+        weekday: "long",
+        month: "numeric",
+        day: "numeric",
+      }),
 
-    row.noEvents = !row.allDay.length && !row.time.length;
+      time: row.time.forEach((ev) => ({
+        ...ev,
+        start: new Date(ev.start).toLocaleString("pl-PL", {
+          timeZone: TIME_ZONE,
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        end: new Date(ev.end).toLocaleString("pl-PL", {
+          timeZone: TIME_ZONE,
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+      })),
 
-    return row;
-  });
+      noEvents: !row.allDay.length && !row.time.length,
+    })
+  );
 }
