bd19fbd9271c9b531869141e8c5019f2744e204e
Adam Bukowski <bukowski.adam@gmail.com>
2024-05-04 22:01:15 +0200

    fix: close browser context and await page goto and warmup in Promise.all

--- src/screenshot.ts
+++ src/screenshot.ts
@@ -1,4 +1,4 @@
-import type { ChromiumBrowser } from "playwright-core";
+import type { BrowserContext, ChromiumBrowser } from "playwright-core";
 import { launchChromium } from "playwright-aws-lambda";
 import axios from "axios";
 
@@ -6,12 +6,13 @@ import type { ScreenshotResult } from "./types.js";
 
 export async function takeScreenshot(url: string): Promise<ScreenshotResult> {
   let browser: ChromiumBrowser | null = null;
+  let context: BrowserContext | null = null;
 
   try {
     const warmupPageCache = axios.get(url);
 
     browser = await launchChromium({ headless: true });
-    const context = await browser.newContext({
+    context = await browser.newContext({
       locale: "pl-PL",
       userAgent:
         "Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36",
@@ -20,9 +21,8 @@ export async function takeScreenshot(url: string): Promise<ScreenshotResult> {
     const page = await context.newPage();
     await page.setViewportSize({ width: 600, height: 800 });
 
-    await warmupPageCache;
+    const [response] = await Promise.all([page.goto(url), warmupPageCache]);
 
-    const response = await page.goto(url);
     await page.addStyleTag({
       content: "* { -webkit-font-smoothing: antialiased; }",
     });
@@ -36,6 +36,9 @@ export async function takeScreenshot(url: string): Promise<ScreenshotResult> {
   } catch (error) {
     throw error;
   } finally {
+    if (context) {
+      await context.close();
+    }
     if (browser) {
       await browser.close();
     }
